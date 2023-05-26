603bc7b701a266a4d9690b3d0143ec05ec91080d
deepakvarughese <69152070+deepakvarughese@users.noreply.github.com>
2023-05-26 14:54:17 +0530

    added second question to the loadingdat.r file

--- loading_data.R
+++ loading_data.R
@@ -2,6 +2,7 @@ library(here)
 library(rio)
 library(janitor)
 library(tidyverse)
+library(mice)
 
 data <- import(here("activity.zip"))
 
@@ -22,3 +23,50 @@ hist(steps_per_day$total)
 
 mean_steps_per_day <- mean(steps_per_day$total, na.rm = TRUE)
 mean_steps_per_day
+
+median_steps_per_day <- median(steps_per_day$total, na.rm = TRUE)
+median_steps_per_day
+
+
+## The 5 minute interval that has on average the most steps taken
+
+most_steps_period <- data %>% 
+  group_by(interval) %>% 
+  summarize(mean_steps_per_interval = mean(steps, na.rm = TRUE)) %>% 
+  arrange(desc(mean_steps_per_interval))
+
+head(most_steps_period, 1)
+
+plot <- ggplot(data = most_steps_period, mapping = aes(x = interval, y = mean_steps_per_interval))+
+                 geom_col()
+plot
+
+
+
+## Missing Data
+
+
+#Method 1 : Using the mice package
+
+md.pattern(data)
+
+tempdata <- mice(data,m=5,maxit=50,meth='pmm',seed=500)
+complete_data <- complete(tempdata,1)
+
+
+steps_per_day_impute <- complete_data %>% 
+  group_by(date) %>% 
+  summarize(total = sum(steps))
+
+##histogram for imputed data
+hist(steps_per_day_impute$total)
+
+#mean and median for imputed data
+mean_steps_per_day_impute <- mean(steps_per_day_impute$total, na.rm = TRUE)
+mean_steps_per_day
+
+median_steps_per_day_impute <- median(steps_per_day_impute$total, na.rm = TRUE)
+median_steps_per_day
+
+
+
